80f07b03e03261b49076d7766b9c1519d7251563
hackerrank <me@hackerrank.com>
2022-11-07 02:12:35 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "https://waysfoodapp.herokuapp.com/api/v1/",
 });
 
 export const setAuthToken = (token) => {
